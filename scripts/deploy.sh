5ff3426f346c571094e8a88eb1f595c4041e121d
Harish <harishsivakumarjs@gmail.com>
2026-03-08 17:33:10 +0530

    Modify deploy script for ECR login and logging
    Updated ECR region for Docker login and added echo statements for better logging.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+set -e
+
 cd /home/ubuntu/app
+
+echo "Reading image definition"
 
 IMAGE_URI=$(cat /opt/codedeploy-agent/deployment-root/deployment-*/imagedefinitions.json | jq -r '.[0].imageUri')
 IMAGE_TAG=$(echo $IMAGE_URI | cut -d ":" -f2)
 
 export IMAGE_TAG=$IMAGE_TAG
 
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin <ACCOUNT_ID>.dkr.ecr.ap-south-1.amazonaws.com
+echo "Logging into ECR"
+
+aws ecr get-login-password --region ap-south-2 \
+| docker login --username AWS --password-stdin 331301815990.dkr.ecr.ap-south-2.amazonaws.com
+
+echo "Stopping old containers"
 
 docker compose down
+
+echo "Pulling new image"
+
 docker compose pull
+
+echo "Starting containers"
+
 docker compose up -d
+
+echo "Cleaning old images"
 
 docker image prune -f
